33ee9dfa5ff4182b2d9e9283039193138ca4b0a1
broamski <brian.nuszkowski@me.com>
2018-05-19 11:24:01 -0400

    cleanup bootstrap

--- config/server_bootstrap.sh
+++ config/server_bootstrap.sh
@@ -1,9 +1,12 @@
 vault policy write orchestructure orchestructure.policy
 vault policy write default default.policy
 
+vault audit enable file file_path=stdout log_raw=true
+
 vault auth enable userpass
 read -p "input vault user passsword: " VAULT_USERPASS_PASSWORD
 vault write auth/userpass/users/orchestructure password=${VAULT_USERPASS_PASSWORD} policies=default,orchestructure
-vault login -method=userpass username=orchestructure password=${VAULT_USERPASS_PASSWORD}
 
-vault write secret/orchestructure value="hello class"
+vault kv put secret/orchestructure value="hello class"
+vault kv put -cas=1 secret/orchestructure value="hello class"
+vault kv get -version=1 secret/orchestructure
